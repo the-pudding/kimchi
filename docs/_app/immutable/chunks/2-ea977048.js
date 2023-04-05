5ad45fd1399cde5b4a0999d16b916f77165574d2
alvinschang <alvinschang@gmail.com>
2023-04-05 09:55:16 -0400

    update github pages

--- docs/_app/immutable/chunks/2-ea977048.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-ea08e456.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-ffea6fbf.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1f549bea.js";export{t as component};
