72b05fbab57a0f6c03df0084226beeb5b9cc3303
russellsamora <russellsamora@gmail.com>
2023-05-10 09:59:31 -0400

    update tip

--- src/components/helpers/Tip.svelte
+++ src/components/helpers/Tip.svelte
@@ -20,9 +20,6 @@
 		box-shadow: 0px 3px 3px 0px var(--color-gray-500);
 		letter-spacing: 0.05em;
 		padding: 12px 16px;
-		margin-left: auto;
-		margin-right: 10px;
-		margin-bottom: 20px;
 		position: absolute;
 		top: 16px;
 		right: 16px;
